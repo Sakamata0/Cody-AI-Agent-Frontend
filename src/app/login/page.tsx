a2f13ccdf835b0bdda1c0f92f8ea55cf2a844b26
sakamata0 <iskanderboughnimi@gmail.com>
2026-07-12 11:52:09 +0100

    fix: remove OAuth debug alert

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -97,7 +97,6 @@ export default function LoginPage() {
   function handleGoogleLogin() {
     const redirectUri = encodeURIComponent(window.location.origin + "/auth/callback");
     const url = `${COGNITO_DOMAIN}/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}&identity_provider=Google&scope=openid+email+profile`;
-    alert(url);
     window.location.href = url;
   }
 
